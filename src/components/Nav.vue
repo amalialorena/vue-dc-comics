dbeb5cb8e06416114017d2c491c0728aa281a789
Amalia <amalialorenaferariu@gmail.com>
2021-11-24 15:34:10 +0100

    nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -35,9 +35,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
     nav {
-        height: 70px;
-        background-color: grey;
         .container {
+            height: 70px;
             display: flex;
             justify-content: space-between;
              width: 70%;
