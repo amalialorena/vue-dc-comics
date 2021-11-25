3b232ca7762aa59bc3a138392312c621393020bc
Amalia <amalialorenaferariu@gmail.com>
2021-11-25 14:38:38 +0100

    cards, dynamic content

--- src/components/Comic.vue
+++ src/components/Comic.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="hello">
-   
+  <div class="card">
+        <img :src="img" alt="">
+        <p>{{title}}</p>
   </div>
 </template>
 
@@ -9,12 +10,16 @@ export default {
   name: 'Comic',
   
   props: {
-    msg: String
+    img: String, 
+    title: String
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+    .card {
+        width: calc((100% / 6) - 10px);
+        margin: 5px;
+    }
 </style>
